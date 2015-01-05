d3be4b7d9fb12dfe0d6dcb005a59de60c0c58756
Andrew McRobb <andrewmcrobb@Fabcoms-iMac.local>
2015-01-05 15:47:32 -0700

    Added example file!

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+	require_once('quickLinkedIn.php');
+
+
+	$link = new quickLinkedIn('', '');
+	$link->setRedirect('http://localhost/quickLinkedInPHP/');
+	
+	if(!isset($_GET['code']))
+	{
+		$link->setScope('r_basicprofile r_emailaddress rw_groups');
+		$url = $link->getAccessUrl();
+		print '<a href="'.$url.'">Login....</a>';
+	}
+	else
+	{
+		$result = $link->auth_code(); //Turns response code into token.
+		
+		
+		if(!$result)
+		{
+			print('Something went wrong! D:');
+			print '<br/><a href="/quickLinkedInPHP/">try again?</a>';
+			die();
+		}
+		
+		//LETS GET DIRTY!
+		//https://developer.linkedin.com/documents/profile-fields
+		var_dump($link->call('people/~:(first-name,last-name)'));
+		var_dump($link->call('people/~/email-address'));
+		var_dump($link->call('people/~/group-memberships'));
+		
+		
+
+	}
+	
+	
+	
+	print '<br/><br/><a href="/quickLinkedInPHP/">try again?</a>';
+	
+	
+
+?>
